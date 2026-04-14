d4ac0ad3b357567cbfa761ee5b4ec72e15382064
BaoyiWan <bawan@ucsd.edu>
2026-04-14 13:10:30 -0700

    revise1 lab3

---
+++ global.js
@@ -0,0 +1,112 @@
+console.log("IT’S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+/*
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+currentLink?.classList.add('current');
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
+}
+*/
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv/', title: 'CV' },
+  { url: 'https://github.com/BaoyiWan/', title: 'Profile'}
+  // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
+
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  if (!url.startsWith('http')) {
+    //console.log(BASE_PATH)
+    //console.log(url)
+    url = BASE_PATH + url;
+  }
+  // next step: create link and add it to nav
+  //nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+//console.log(a)
+//console.log('hello')
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector("#theme-select");
+
+if ("colorScheme" in localStorage) {
+  const saved = localStorage.colorScheme;
+  document.documentElement.style.setProperty("color-scheme", saved);
+  select.value = saved;
+}
+select.addEventListener('input', function (event) {
+  const value = event.target.value;
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+  console.log('color scheme changed to', event.target.value);
+});
+
+const form = document.querySelector("form");
+
+form?.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+  const data = new FormData(form);
+
+  let url = form.action + "?"; 
+
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+  }
+
+  url += params.join("&");
+
+  console.log(url); // debug
+
+  location.href = url; 
+});
+
+
