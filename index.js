b7e3465f90f30f42f2960a3eb39962a75cf053db
BaoyiWan <bawan@ucsd.edu>
2026-04-23 16:00:22 -0700

    lab4 revise4

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { fetchJSON, renderProjects, fetchGitHubData} from './global.js'; //no fetchGithubData????
-const projects = await fetchJSON('./projects/lib/projects.json'); //changed??? index.js position correct???
+import { fetchJSON, renderProjects, fetchGitHubData} from './global.js'; 
+const projects = await fetchJSON('./projects/lib/projects.json'); 
 const githubData = await fetchGitHubData('BaoyiWan');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 const profileStats = document.querySelector('#profile-stats');
 renderProjects(latestProjects, projectsContainer, 'h2');
 
-if (profileStats) { //change when updated each time?
+if (profileStats) {
   profileStats.innerHTML = `
         <dl>
-          <dt>Public Repos:</dt><dd>8</dd>
-          <dt>Public Gists:</dt><dd>0</dd> 
-          <dt>Followers:</dt><dd>0</dd>
-          <dt>Following:</dt><dd>0</dd>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
         </dl>
     `;
 }
