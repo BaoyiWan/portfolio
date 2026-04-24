40dcb71d16054d63f8585483855ebbc7abe20ffe
BaoyiWan <bawan@ucsd.edu>
2026-04-24 00:30:43 -0700

    lab4 revise6

--- global.js
+++ global.js
@@ -125,10 +125,11 @@ export async function fetchJSON(url) {
 
 export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
-  projects.forEach(project => { //loop like this???
+  projects.forEach(project => { 
     const article = document.createElement('article');
     article.innerHTML = `
-      <h3>${project.title}</h3>
+      <${headingLevel}>${project.title}</${headingLevel}> 
+      <p class="project-year">${project.year}</p>
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
     `;
