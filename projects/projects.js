0057d9599a9af08c6cda60e338cb3c991b173070
BaoyiWan <bawan@ucsd.edu>
2026-04-30 23:56:12 -0700

    lab5 revise3

--- projects/projects.js
+++ projects/projects.js
@@ -61,6 +61,7 @@ searchInput.addEventListener('change', (event) => {
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 let currentData = [];
 let selectedIndex = -1;
+let query = ''; //add1
 function renderPieChart(projectsGiven) {
   // re-calculate rolled data
   let newRolledData = d3.rollups(
@@ -90,6 +91,7 @@ function renderPieChart(projectsGiven) {
       .append('path')
       .attr('d', arc)
       .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
+      .attr('class', idx === selectedIndex ? 'selected' : '') //add4
       .on('click', () => {
       // What should we do? (Keep scrolling to find out!)
       selectedIndex = selectedIndex === idx ? -1 : idx;
@@ -103,27 +105,45 @@ function renderPieChart(projectsGiven) {
             // TODO: filter idx to find correct legend and apply CSS from above
             idx === selectedIndex ? 'selected' : 'legend-item'
           );
-      if (selectedIndex === -1) {
-          renderProjects(projectsGiven, projectsContainer, 'h2');
-        } else {
-          let selectedYear = currentData[selectedIndex].label;
-          let filtered = projectsGiven.filter(p =>
-            p.year === selectedYear
-          );
-
-          renderProjects(filtered, projectsContainer, 'h2'); 
-        }
+      // if (selectedIndex === -1) {
+      //     renderProjects(projectsGiven, projectsContainer, 'h2');
+      //   } else {
+      //     let selectedYear = currentData[selectedIndex].label;
+      //     let filtered = projectsGiven.filter(p =>    
+      //       p.year === selectedYear
+      //     );
+      //     renderProjects(filtered, projectsContainer, 'h2'); 
+      //   }
+        applyFilters();//revise3
     });
   })
+
 
   newData.forEach((d, idx) => {
     legend
       .append('li')
       .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-      .attr('class', 'legend-item')
+      //.attr('class', 'legend-item')
+      .attr('class', idx === selectedIndex ? 'selected' : 'legend-item') //add5
       .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
   });
 }
+
+//revise3
+function applyFilters() {
+  let filteredProjects = projects.filter((project) => {
+    let matchSearch = Object.values(project).join('\n').toLowerCase().includes(query.toLowerCase());
+    
+    if (selectedIndex === -1) {
+      return matchSearch;
+    } else {
+      let selectedYear = currentData[selectedIndex].label;
+      return matchSearch && project.year === selectedYear;
+    }
+  });
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+}
+
 
 let searchInput = document.querySelector('.searchBar');
 renderPieChart(projects);
@@ -131,17 +151,25 @@ renderProjects(projects, projectsContainer, 'h2');
 
 searchInput.addEventListener('input', (event) => {
   // update query value
-  let query = event.target.value;
+  query = event.target.value; //revise5
   // TODO: filter the projects
+  let selectedLabel = selectedIndex !== -1 ? currentData[selectedIndex]?.label : null;//add5
+
   let filteredProjects = projects.filter((project) => {
     let values = Object.values(project).join('\n').toLowerCase();
     return values.includes(query.toLowerCase());
   });
 
-  selectedIndex = -1;
+  //selectedIndex = -1; revise2
   // re-render legends and pie chart when event triggers
-  renderProjects(filteredProjects, projectsContainer, 'h2');
+  //renderProjects(filteredProjects, projectsContainer, 'h2'); revise4
   renderPieChart(filteredProjects);
+
+  if (selectedLabel) {
+    selectedIndex = currentData.findIndex(d => d.label === selectedLabel);
+  }
+  
+  applyFilters();//revise3
 });
 
 
